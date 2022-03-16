05506fe17ffe408a5d441fe024ee485513f63d38
ilpanda <litencent@gmail.com>
2022-03-16 22:13:20 +0800

    【update】kotlin 版本降级，新建 epic-demo 工程。

---
+++ arch-epic-demo/src/main/java/com/ilpanda/arch/epic/demo/hook/BaseTraceHook.kt
@@ -0,0 +1,12 @@
+package com.ilpanda.arch.epic.demo.hook
+
+import android.util.Log
+import de.robv.android.xposed.XC_MethodHook
+
+open class BaseTraceHook(val logTag: String = "") : XC_MethodHook() {
+
+    override fun beforeHookedMethod(param: MethodHookParam?) {
+        super.beforeHookedMethod(param)
+        Log.i("EpicHook: $logTag", Log.getStackTraceString(RuntimeException()))
+    }
+}
---
+++ arch-epic-demo/src/main/java/com/ilpanda/arch/epic/demo/hook/EpicInit.kt
@@ -0,0 +1,74 @@
+package com.ilpanda.arch.epic.demo.hook
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.telephony.TelephonyManager
+
+object EpicInit {
+
+    @JvmStatic
+    fun init(context: Context, epicConfig: EpicConfig) {
+
+        // 1. 模拟器不使用 epic
+        // 2. 正式发布不使用 epic
+        if (isEmulator(context.applicationContext) || !epicConfig.isDebug) {
+            return
+        }
+
+        initInnerHandler(epicConfig)
+
+        installCustomHandler(epicConfig)
+    }
+
+    private fun initInnerHandler(epicConfig: EpicConfig) {
+        if (epicConfig.installDialog) {
+            epicConfig.addHandler(DialogHandler())
+        }
+
+        if (epicConfig.installToast) {
+            epicConfig.addHandler(ToastHandler())
+        }
+
+        if (epicConfig.installPopupWindow) {
+            epicConfig.addHandler(PopupWindowHandler())
+        }
+
+        if (epicConfig.installActivity) {
+            epicConfig.addHandler(ActivityHandler())
+        }
+    }
+
+    private fun installCustomHandler(epicConfig: EpicConfig) {
+        epicConfig.handle()
+    }
+
+    fun isEmulator(context: Context): Boolean {
+        val checkProperty = (Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.toLowerCase().contains("vbox")
+                || Build.FINGERPRINT.toLowerCase().contains("test-keys")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+                || "google_sdk" == Build.PRODUCT)
+        if (checkProperty) return true
+        var operatorName = ""
+        val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val name = tm.networkOperatorName
+        if (name != null) {
+            operatorName = name
+        }
+        val checkOperatorName = operatorName.toLowerCase() == "android"
+        if (checkOperatorName) return true
+        val url = "tel:" + "123456"
+        val intent = Intent()
+        intent.data = Uri.parse(url)
+        intent.action = Intent.ACTION_DIAL
+        val checkDial = intent.resolveActivity(context.packageManager) == null
+        return checkDial
+    }
+}
+
